7afa2891ccc9ce1eb9e1506f40206e8329030136
Abdelati Elshafi <abdelati.elshafi@retailtec-egypt.com>
2026-03-31 14:12:31 +0200

    edit server ip

--- lib/backend/api_requests/api_calls.dart
+++ lib/backend/api_requests/api_calls.dart
@@ -12,7 +12,7 @@ const _kPrivateApiFunctionName = 'ffPrivateApiCall';
 
 class TakeANumberAPIGroupGroup {
   static String getBaseUrl() =>
-      'https://47c4-197-36-207-117.ngrok-free.app/BackEnd_Server';
+      'http://10.0.36.88:8002/BackEnd_Server';
   static Map<String, String> headers = {};
   static GetDepartServingnoCall getDepartServingnoCall =
       GetDepartServingnoCall();
